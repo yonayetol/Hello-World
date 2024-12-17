09efeb7d6e5bdc9a23fcce61ae76dbcc4e223a99
yonayetol <japutest6@gmail.com>
2024-12-17 04:18:31 +0300

    Time: 5 ms (52.03%) | Memory: 17.3 MB (25.74%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        the_copy = sorted(nums[:])
-#         1 2 2 3 8
-        final =[]
-        for i in nums:
-            location = the_copy.index(i)# the place of i in the_copy
-            final.append(location)
-        return final
+        other = [0]*(max(nums)+1)
+        for i in range(len(nums)):
+            print("now:",i)
+            other[nums[i]] += 1
+        new_dict,place = {},0
+        for i in range(len(other)):
+            if other[i]>0:
+                new_dict[i] = place
+                place += other[i]
+                
+        for i in range(len(nums)):
+            nums[i] = new_dict[nums[i]]
+        return nums
+
+       
