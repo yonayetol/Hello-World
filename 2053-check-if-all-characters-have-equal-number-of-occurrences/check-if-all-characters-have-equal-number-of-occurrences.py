24fdc216a3c4225d88273d11ada6f8406b7c13f3
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-06 09:51:38 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (69.70%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,21 +1,3 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        letters = [[chr(i),0] for i in range(97, 123)]
-        count = 0
-        for char in s:
-            for i in range(len(letters)):
-                if letters[i][0] == char:
-                    letters[i][1] += 1
-                    break
-        
-        for i in range(26):
-            if letters[i][1] != 0:
-                count = letters[i][1] 
-                break
-
-        for i in range(len(letters)):
-            if letters[i][1] != 0:
-                if letters[i][1] != count:
-                    return False
-
-        return True
+        return len(set(Counter(s).values())) == 1
