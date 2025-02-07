134c539aa5471971885424df9677d68e8ddb8a10
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-07 08:03:46 +0300

    Time: 8 ms (28.13%) | Memory: 18 MB (18.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,18 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        my_dict={
-                    "I":1,
-                    "V":5,
-                    "X":10,
-                    "L":50,
-                    "C":100,
-                    "D":500,
-                    "M":1000,
-                }
-        final=[my_dict[char] for char in s]
-        sum=final[-1]
-        for n in range(len(s)-1):
-            sum+=final[n]
-            if final[n]<final[n+1]:
-                sum-=(final[n]*2)
-        return sum
+        my_dict = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        runing = 0
+        for i in range(len(s)):
+            runing += my_dict[s[i]]
+            if i > 0 and my_dict[s[i]] > my_dict[s[i-1]]:
+                runing -= 2*my_dict[s[i-1]]
+        return runing
