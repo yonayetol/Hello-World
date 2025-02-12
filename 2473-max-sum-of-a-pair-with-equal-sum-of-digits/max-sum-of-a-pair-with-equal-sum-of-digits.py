ef9c7244435e8873ee42abef8cb7e23f24000941
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-12 10:25:10 +0300

    Time: 336 ms (57.36%) | Memory: 33.7 MB (33.47%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -6,10 +6,13 @@ class Solution:
                 sums+=(num%10)
                 num//=10
             return sums
+        
         my_sorting = [0]*82
         answer = -1
+
         for i in range(len(nums)):
             little_sum = digit_sum(nums[i])
+            
             if my_sorting[little_sum] == 0:
                 my_sorting[little_sum] = [nums[i]]
                 heapify(my_sorting[little_sum])
@@ -18,4 +21,5 @@ class Solution:
                 if len(my_sorting[little_sum]) > 2:
                     heappop(my_sorting[little_sum])
                 answer = max(answer,my_sorting[little_sum][0]+my_sorting[little_sum][1])
+
         return answer
