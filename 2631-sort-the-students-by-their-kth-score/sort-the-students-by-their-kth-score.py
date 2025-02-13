61e1369dde85e8a7a2c7587488a59fb648b0b91d
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-13 12:11:58 +0300

    Time: 0 ms (100.00%) | Memory: 22.6 MB (49.49%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,12 +1,3 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        chosen = {}
-        for i,e in enumerate(score):
-            chosen[e[k]] = i
-        chosen = sorted(chosen.items())
-        new = []
-        for i,e in chosen:
-            new.append(score[e])
-        new.reverse()
-        return new
-                
+        return sorted(score, key = lambda x:x[k], reverse = True)
