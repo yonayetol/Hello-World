0102d70957bd03c3793b7e199a230c9745298d5d
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-16 09:45:09 +0300

    Time: 386 ms (52.24%) | Memory: 33.5 MB (67.11%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -6,10 +6,9 @@ class Solution:
                 sums+=(num%10)
                 num//=10
             return sums
-        
+
         my_sorting = [0]*82
         answer = -1
-
         for i in range(len(nums)):
             little_sum = digit_sum(nums[i])
             
