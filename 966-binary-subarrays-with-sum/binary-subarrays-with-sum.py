904ae774a55d1e084191a059129ad0eb175c444b
Yonas Ayele Tola <japutest6@gmail.com>
2025-02-18 16:38:47 +0300

    Time: 38 ms (22.80%) | Memory: 21.5 MB (32.86%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -3,6 +3,10 @@ class Solution:
         my_dict,count,running  = Counter({0:1}),0,0
         for num in nums:
             running += num
-            count += my_dict.get(running-goal,0) 
-            my_dict[running] += 1
+            if running-goal in my_dict:
+                count += my_dict[running-goal]
+            if running not in my_dict:
+                my_dict[running] = 1
+            else:
+                my_dict[running] += 1
         return count
