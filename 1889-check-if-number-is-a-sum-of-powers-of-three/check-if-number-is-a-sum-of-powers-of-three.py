53cb42ce6b705935302c28c8b7fb8ca8ab14990d
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-04 06:24:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.84%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        tempo = 3
+        while tempo*3 <= n:
+            tempo *= 3
+
+        while tempo > 0:
+            while n - tempo < 0:
+                tempo //= 3
+            n -= tempo
+            tempo //= 3
+            
+        return n == 0 
