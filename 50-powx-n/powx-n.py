bc987b8c2cde63804d92cfede159efcba3beb629
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-12 18:55:44 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (31.90%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,13 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            n,x = -n,1/x
-        def other(x,n):
-            if n == 0:
-                return 1
-            even = (n%2 == 0)
-            if even:
-                return other(x,n//2)**2
-            else:
-                return other(x,n//2)**2*x
-        return other(x,n)
+        reminder = {}
+        def other(x,m):
+            if m == 0: return 1
+            if m == 1: return x
+
+            if ceil(m/2) not in reminder: reminder[ceil(m/2)] = other(x,ceil(m/2))
+            if m//2 not in reminder: reminder[m//2] = other(x,m//2)
+                
+            return reminder[ceil(m/2)] * reminder[m//2]
+
+        ans = other(x,abs(n))
+        if n < 0:
+            return 1/ans
+        return ans
