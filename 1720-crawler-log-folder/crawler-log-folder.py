122e5c55d6c626c03687d3a363beca1e65c038c8
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-13 19:57:41 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.98%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for i in range(len(logs)):
+            if logs[i] == "../" and count > 0: count -= 1
+            elif logs[i] != "./" and logs[i] != "../" : count += 1
+        return count
+        
