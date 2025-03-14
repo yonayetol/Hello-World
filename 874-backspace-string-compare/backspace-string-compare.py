083eb25df0131633a34167097d347346b2750529
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-14 05:19:37 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.47%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,11 +1,20 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def correcter(word):
-            stack = []
-            for char in word:
-                if char != "#":
-                    stack.append(char)
-                elif stack:
-                    stack.pop()
-            return stack
-        return correcter(s)==correcter(t)
+        ptrs, ptrt = len(s)-1, len(t)-1
+        while  ptrs > -1 or ptrt > -1:
+            count = 0
+            while ptrs > -1 and (count > 0 or s[ptrs] == "#"):
+                count += 1 if s[ptrs] == "#" else -1
+                ptrs -= 1
+
+            count = 0
+            while ptrt > -1 and (count > 0 or t[ptrt] == "#"):
+                count += 1 if t[ptrt] == "#" else -1
+                ptrt -= 1
+                
+            if ptrs != -1 and ptrt != -1 and t[ptrt] == s[ptrs]:
+                ptrs -= 1
+                ptrt -= 1
+            elif (ptrs <= -1 and ptrt != -1) or (ptrs != -1 and ptrt <= -1) or (ptrs!=-1 and ptrt!= -1 and s[ptrs] != t[ptrt]): return False
+
+        return True
