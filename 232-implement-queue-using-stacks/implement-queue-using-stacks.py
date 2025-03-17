fb4a160e9e15a5a00612ee9295676862d3ea138a
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-17 18:04:51 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.74%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,24 @@
+class MyQueue:
+
+    def __init__(self):
+        self.que = deque()
+
+    def push(self, x: int) -> None:
+        self.que.append(x)
+
+    def pop(self) -> int:
+        return self.que.popleft()
+
+    def peek(self) -> int:
+        return self.que[0]
+
+    def empty(self) -> bool:
+        return not self.que
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
