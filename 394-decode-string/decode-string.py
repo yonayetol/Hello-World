afd9ae2419a18fbd2cf79548799124e1bf357406
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-19 09:53:29 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (55.81%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,25 +1,28 @@
-from collections import deque
 class Solution:
     def decodeString(self, s: str) -> str:
-        n = len(s)
-        stack = deque()
-        num = 0
-        builtString = ""
-        
-        for i in range(n):
-            char = s[i]
-            if char.isdigit():
-                num = num * 10 + int(char)
-            elif char == '[':
-                stack.append([builtString, num])
-                builtString = ""
-                num = 0
-            elif char == ']':
-                prevStr, repeatTimes = stack.pop()
-                if len(builtString) > 1000000:
-                    return "Output too large! Skipping expansion."
-                builtString = prevStr + (builtString * repeatTimes)
-            else:
-                builtString += char
-            
-        return builtString        
+        idx = 0
+        def helper(idx):
+            stack = []
+            while idx < len(s):
+                if s[idx].isdigit():
+                    newidx = idx
+
+                    while idx<len(s) and s[idx].isdigit(): idx += 1
+                    num = int(s[newidx:idx])
+
+                    ans = helper(idx)
+                    stack.extend(ans[0]*num)
+                    idx = max(ans[1],idx)
+                    
+                elif s[idx] != "[" and s[idx] != "]": stack.append(s[idx])
+                elif s[idx] == "]": return ["".join(stack), idx]
+                idx += 1
+
+            return ["".join(stack), idx]
+
+        total = []
+        while idx < len(s):
+            ans = helper(idx)
+            idx = ans[1]+1
+            total.extend(ans[0])
+        return "".join(total)
