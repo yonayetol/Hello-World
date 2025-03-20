3d1650a2bd99e47d77ef27358ee5d6da93bbd365
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-20 19:46:55 +0300

    Time: 12 ms (40.81%) | Memory: 17.9 MB (61.95%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = []
+        tempo = []
+        candidates.sort()
+        def backTrack(i,currSum):
+            if currSum == target:
+                answer.append(tempo.copy())
+            if currSum >= target: return
+
+            for j in range(i,len(candidates)):
+                tempo.append(candidates[j])
+                currSum += candidates[j]
+                if currSum == target:
+                    answer.append(tempo.copy())
+                if currSum < target:
+                    backTrack(j,currSum)
+                
+                tempo.pop()
+                currSum -= candidates[j]
+
+        backTrack(0,0)
+        return answer
