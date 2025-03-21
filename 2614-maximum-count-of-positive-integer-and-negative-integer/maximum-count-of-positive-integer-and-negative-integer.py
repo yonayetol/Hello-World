db8b254680c5c907660f1ddf8a227cd378e725b1
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-21 09:04:45 +0300

    Time: 0 ms (100.00%) | Memory: 20.2 MB (22.47%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,10 +1,27 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        countPositive = 0
-        countZero = 0
-        for num in nums:
-            if num > 0:
-                countPositive += 1
-            elif num == 0:
-                countZero += 1
-        return max(countPositive, len(nums)-countPositive-countZero)
+        if nums[0]==nums[-1]==0:return 0
+        posptr = (len(nums))//2 -1
+        def pos_track(posptr,n):
+            n = ceil(n/2)            
+            if nums[posptr] > 0:
+                if posptr <= 0 or nums[posptr-1] <=0:
+                    return max(0,posptr)
+                return pos_track(posptr-n,n)
+            else:
+                if posptr >= len(nums)-1 or nums[posptr + 1] > 0:
+                    return min(len(nums)-1,posptr + 1)
+                return pos_track(posptr + n,n) 
+
+        def neg_track(posptr,n):
+            n = ceil(n/2)
+            if nums[posptr] < 0:
+                if posptr >= len(nums)-1 or nums[posptr+1] >=0:
+                    return min(posptr,len(nums)-1)
+                return neg_track(posptr + n,n)   
+            else:
+                if posptr <= 0 or nums[posptr - 1] < 0:
+                    return max(posptr - 1,0)
+                return neg_track(posptr - n, n)
+                
+        return max(len(nums)-pos_track(posptr, ceil(len(nums)/2)),1+neg_track(posptr, ceil(len(nums)/2)))
