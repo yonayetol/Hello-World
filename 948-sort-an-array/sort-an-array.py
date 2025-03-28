d4dd6a84eddc8aa1ac2ea1878ee114917a17a7f8
Yonas Ayele Tola <japutest6@gmail.com>
2025-03-28 19:30:56 +0300

    Time: 633 ms (54.38%) | Memory: 26.4 MB (21.14%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,10 +1,19 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        counting = [0]*(10**5 + 1)
-        for i in nums:
-            counting[i+50000] += 1
-        other = []
-        for i in range(len(counting)):
-            if counting[i] > 0:
-                other.extend([i-50000]*counting[i])
-        return other
+        def combine(arrA,arrB):
+            answer = []
+            left1 = left2 = 0
+            while left1 < len(arrA) and left2 < len(arrB):
+                if arrA[left1] < arrB[left2]: 
+                    answer.append(arrA[left1])
+                    left1 += 1
+                else: 
+                    answer.append(arrB[left2])
+                    left2 += 1
+            return answer+arrA[left1:]+arrB[left2:]
+
+        def divide(left, right, nums):
+            if left == right : return [nums[left]]
+            mid = (left+right)//2
+            return combine(divide(left, mid, nums), divide(mid+1, right, nums))
+        return divide(0,len(nums)-1, nums)
